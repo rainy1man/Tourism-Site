75822bf02f515d79e75f8a2e9145f7b4ec6cdf0c
mohammad <mohammadmokhtari.a1@gmail.com>
2024-07-02 10:25:51 +0330

    add type field in setting table

--- database/migrations/2024_06_29_152736_create_settings_table.php
+++ database/migrations/2024_06_29_152736_create_settings_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('key');
             $table->longText('value');
+            $table->enum('type', ['string', 'json']);
             $table->timestamps();
         });
     }
