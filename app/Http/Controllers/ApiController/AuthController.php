1ff2a28a0cd58b606f0c40d19459e9c483292c83
Mohammad Amin <mhmdghrmn78@gmail.com>
2024-07-07 09:42:32 +0330

    add login

--- app/Http/Controllers/ApiController/AuthController.php
+++ app/Http/Controllers/ApiController/AuthController.php
@@ -1,7 +1,8 @@
 <?php
 
 namespace App\Http\Controllers\ApiController;
-
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
@@ -10,9 +11,10 @@ class AuthController extends Controller
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function role_index()
     {
-        //
+        $roles = Role::all();
+        return response()->json($roles, 200);
     }
 
     /**
@@ -45,5 +47,26 @@ class AuthController extends Controller
     public function destroy(string $id)
     {
         //
+    }
+    public function login (Request $request){
+        $user = User::where('email', $request->email)
+            ->first();
+        if (!$user) {
+            return response()->json('user not found');
+        }
+
+        if (!Hash::check($request->password, $user->password)) {
+            return response()->json('pass eror');
+        }
+
+        $token = $user->createToken($request->email)->plainTextToken;
+
+        return response()->json(["token" => $token]);
+    }
+
+    public function logout(Request $request){
+        $request->user()->currentAccessToken()->delete();
+        return response()->json('logged out');
+
     }
 }
