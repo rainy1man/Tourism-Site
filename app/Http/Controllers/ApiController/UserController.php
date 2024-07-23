abd06a40e3b747b081459557074f0666fb725305
mohammad <mohammadmokhtari.a1@gmail.com>
2024-07-23 16:12:16 +0330

    Editing user crud functions

--- app/Http/Controllers/ApiController/UserController.php
+++ app/Http/Controllers/ApiController/UserController.php
@@ -9,62 +9,62 @@ use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
-    public function index (){
-        $user = User::all();
-        return response()->json($user);
-    }
-
-    public function store (Request $request){
-        $user = User::create($request->toArray());
-        return response()->json($user);
-    }
-
-    public function show (string $id){
-        $user = User::find($id);
-        if(!$user) {
-            return response()->json(['message' => 'User not found'], 404);
+    //
+    public function index(Request $request)
+    {
+        if ($request->user()->can('see.user')) {
+            $users = new User();
+            $users = $users->orderBy('id', 'desc')->paginate(10);
+            return $this->responseService->success_response($users);
+        } else {
+            return $this->responseService->unauthorized_response();
         }
-        return response()->json($user);
     }
 
+    //
+    public function store(Request $request)
+    {
+        if ($request->user()->can('create.user')) {
+            $national_code = $request->input('national_code');
+            $password = Hash::make($national_code);
+            $user = User::create($request->merge(["password" => $password])->toArray());
+            $user->assignRole('user');
+            return $this->responseService->success_response($user);
+        } else {
+            return $this->responseService->unauthorized_response();
+        }
+    }
+
+    //
+    public function show(Request $request, string $id)
+    {
+        if ($request->user()->can('see.user') || $request->user()->id == $id) {
+            $user = User::find($id);
+            return $this->responseService->success_response($user);
+        } else {
+            return $this->responseService->unauthorized_response();
+        }
+    }
+
+    //
     public function update(Request $request, string $id)
     {
-        $user = User::find($id);
-        if(!$user) {
-            return response()->json(['message' => 'User not found'], 404);
+        if ($request->user()->can('update.user') || $request->user()->id == $id) {
+            $user = User::where('id', $id)->update($request->merge(["password" => Hash::make($request->password)])->toArray());
+            return $this->responseService->success_response($user);
+        } else {
+            return $this->responseService->unauthorized_response();
         }
-        $user->update($request->toArray());
-        return response()->json($user);
     }
 
-    public function delete (Request $request,$id){
-        $user = User::find($id);
-        if(!$user) {
-            return response()->json(['message' => 'User not found'], 404);
+    //
+    public function destroy(Request $request, $id)
+    {
+        if ($request->user()->can('delete.user')) {
+            User::destroy($id);
+            return $this->responseService->delete_response('کاربر');
+        } else {
+            return $this->responseService->unauthorized_response();
         }
-        $user->delete();
-        return response()->json(['message' => 'User deleted successfully']);
-    }
-
-    public function login (Request $request){
-        $user = User::where('email', $request->email)
-            ->first();
-        if (!$user) {
-            return response()->json('user not found');
-        }
-
-        if (!Hash::check($request->password, $user->password)) {
-            return response()->json('pass eror');
-        }
-
-        $token = $user->createToken($request->email)->plainTextToken;
-
-        return response()->json(["token" => $token]);
-    }
-
-    public function logout(Request $request){
-        $request->user()->currentAccessToken()->delete();
-        return response()->json('logged out');
-
     }
 }
