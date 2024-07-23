155125c1f5264b3fff4317dfff13fa6b49c53b5a
mohammad <mohammadmokhtari.a1@gmail.com>
2024-07-23 15:57:47 +0330

    Editing and user and auth routes

--- routes/api/UserRoutes.php
+++ routes/api/UserRoutes.php
@@ -3,9 +3,14 @@
 use App\Http\Controllers\ApiController\UserController;
 use Illuminate\Support\Facades\Route;
 
+Route::middleware('auth:sanctum')->group(function () {
+
 Route::group(['prefix' => 'users', 'as' => 'users.'], function () {
-    Route::get('index/{id?}', [UserController::class, 'index'])->name('index');
-    Route::post('store', [UserController::class, 'store'])->name('store');
-    Route::put('update/{id}', [UserController::class, 'update'])->name('update');
-    Route::delete('destroy/{id}', [UserController::class, 'destroy'])->name('destroy');
+    Route::get('/', [UserController::class, 'index'])->name('index');
+    Route::post('/', [UserController::class, 'store'])->name('store');
+    Route::get('{id}', [UserController::class, 'show'])->name('show');
+    Route::put('{id}', [UserController::class, 'update'])->name('update');
+    Route::delete('{id}', [UserController::class, 'destroy'])->name('destroy');
+});
+
 });
