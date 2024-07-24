549c108273dc92f8af14ee629f4c03e22b9bef46
Mohammad Amin <mhmdghrmn78@gmail.com>
2024-07-24 05:41:29 +0330

    edit order table

--- database/migrations/2024_06_29_152635_create_orders_table.php
+++ database/migrations/2024_06_29_152635_create_orders_table.php
@@ -16,7 +16,8 @@ return new class extends Migration
             $table->foreignId('user_id')->constrained();
             $table->foreignId('trip_id')->constrained();
             $table->integer('price');
-            $table->integer('people_number');
+            $table->integer('adults_number');
+            $table->integer('children_number')->nullable();
             $table->enum('order_status', ['pending', 'completed', 'canceled'])->default('pending');
             $table->enum('payment_status', ['paid', 'unpaid']);
             $table->enum('refund_status', ['not_requested', 'requested', 'processed'])->default('not_requested');
