96c8baacd3f6ccdee3bad141d21cd77946387419
mohammad <mohammadmokhtari.a1@gmail.com>
2024-07-25 15:49:27 +0330

    Add toure and trip routes

--- routes/api/TripRoutes.php
+++ routes/api/TripRoutes.php
@@ -1,3 +1,12 @@
 <?php
 
+use App\Http\Controllers\ApiController\TripController;
 use Illuminate\Support\Facades\Route;
+
+Route::group(['prefix' => 'trip', 'as' => 'trip.'], function () {
+    Route::get('/', [TripController::class, 'index'])->name('index');
+    Route::post('/', [TripController::class, 'store'])->name('store')->middleware('auth:sanctum');
+    Route::get('{id}', [TripController::class, 'show'])->name('show');
+    Route::put('{id}', [TripController::class, 'update'])->name('update')->middleware('auth:sanctum');
+    Route::delete('{id}', [TripController::class, 'destroy'])->name('destroy')->middleware('auth:sanctum');
+});
