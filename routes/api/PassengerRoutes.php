7487c55d932aa8df90473dc0ee6267726f3cb890
mohammad <mohammadmokhtari.a1@gmail.com>
2024-07-29 12:19:44 +0330

    add passenger routes

--- routes/api/PassengerRoutes.php
+++ routes/api/PassengerRoutes.php
@@ -1,3 +1,16 @@
 <?php
 
+use App\Http\Controllers\ApiController\PassengerController;
 use Illuminate\Support\Facades\Route;
+
+Route::middleware('auth:sanctum')->group(function () {
+
+Route::group(['prefix' => 'passenger', 'as' => 'passenger.'], function () {
+    Route::get('/{user?}', [PassengerController::class, 'index'])->name('index');
+    Route::post('/', [PassengerController::class, 'store'])->name('store');
+    Route::get('{id}', [PassengerController::class, 'show'])->name('show');
+    Route::put('{id}', [PassengerController::class, 'update'])->name('update');
+    Route::delete('{id}', [PassengerController::class, 'destroy'])->name('destroy');
+});
+
+});
