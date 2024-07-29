5df9291323da24719c6750c095195edbe117c1a3
Mohammad Amin <mhmdghrmn78@gmail.com>
2024-07-29 11:50:39 +0330

    delete score routs

--- routes/api/ScoreRoutes.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\ScoreController;
-
-Route::middleware('auth:sanctum')->group(function () {
-    Route::post('/scores', [ScoreController::class, 'store']);
-});
