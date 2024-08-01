0d060d972254ea6049020911b08eece9a54e3da3
mohammad <mohammadmokhtari.a1@gmail.com>
2024-08-01 15:31:55 +0330

    create resource for trip section

---
+++ app/Http/Resources/TripDetailResource.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class TripDetailResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            "id" => $this->id,
+            "tour_detail" => new TourResource($this->tour),
+            "price" => $this->price,
+            "discount_price" => $this->discount_price,
+            "capacity" => $this->capacity,
+            "start_at" => $this->start_at,
+            "end_at" => $this->end_at,
+            "meal" => $this->id
+        ];
+    }
+}
