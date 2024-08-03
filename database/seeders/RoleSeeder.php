ef67d8bf7b26133cb18eb778f7cb7ecdfbd6b968
mohammad <mohammadmokhtari.a1@gmail.com>
2024-08-03 22:27:26 +0330

    update role seeder

--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -27,31 +27,103 @@ class RoleSeeder extends Seeder
         // Create super_admin role and assign permissions to this
         $super_admin = Role::create(['name' => 'super_admin']);
         $super_admin->givePermissionTo([
-            'see.user', 'create.user', 'update.user', 'delete.user',
-            'see.trip', 'create.trip', 'update.trip', 'delete.trip',
-            'see.passenger', 'create.passenger', 'update.passenger', 'delete.passenger',
-            'see.order', 'create.order', 'update.order', 'delete.order',
-            'see.comment', 'create.comment', 'update.comment', 'delete.comment',
-            'see.score', 'create.score', 'update.score', 'delete.score',
-            'see.favorite', 'create.favorite', 'update.favorite', 'delete.favorite',
-            'see.discount', 'create.discount', 'update.discount', 'delete.discount',
-            'see.tour', 'create.tour', 'update.tour', 'delete.tour',
-            'see.detail', 'create.detail', 'update.detail', 'delete.detail',
-            'see.refund', 'create.refund', 'update.refund', 'delete.refund',
-            'see.discount', 'create.discount', 'update.discount', 'delete.discount',
+            'see.user',
+            'create.user',
+            'update.user',
+            'delete.user',
+            'see.trip',
+            'create.trip',
+            'update.trip',
+            'delete.trip',
+            'see.passenger',
+            'create.passenger',
+            'update.passenger',
+            'delete.passenger',
+            'see.order',
+            'create.order',
+            'update.order',
+            'delete.order',
+            'see.comment',
+            'create.comment',
+            'update.comment',
+            'delete.comment',
+            'see.score',
+            'create.score',
+            'update.score',
+            'delete.score',
+            'see.favorite',
+            'create.favorite',
+            'update.favorite',
+            'delete.favorite',
+            'see.discount',
+            'create.discount',
+            'update.discount',
+            'delete.discount',
+            'see.tour',
+            'create.tour',
+            'update.tour',
+            'delete.tour',
+            'see.detail',
+            'create.detail',
+            'update.detail',
+            'delete.detail',
+            'see.refund',
+            'create.refund',
+            'update.refund',
+            'delete.refund',
+            'see.discount',
+            'create.discount',
+            'update.discount',
+            'delete.discount',
         ]);
 
         // Create a super_admin user
-        $super_admin = User::create([
-            'first_name' => 'super',
-            'last_name' => 'admin',
-            'phone_number' => '09121234567',
-            'email' => 'example@gmail.com',
-            'password' => '11aa!!AA'
+        $super_admin1 = User::create([
+            'first_name' => 'mohammad',
+            'last_name' => 'mokhtari',
+            'phone_number' => '9390071638',
+            'email' => 'mohammad@gmail.com',
+            'password' => 'Momo1275'
         ]);
 
         // Assign super_admin role to the new user
-        $super_admin->assignRole('super_admin');
+        $super_admin1->assignRole('super_admin');
+
+        // Create a super_admin user
+        $super_admin2 = User::create([
+            'first_name' => 'mohammad amin',
+            'last_name' => 'ghahremani',
+            'phone_number' => '9940362007',
+            'email' => 'mohammadamin@gmail.com',
+            'password' => 'Amin1234'
+        ]);
+
+        // Assign super_admin role to the new user
+        $super_admin2->assignRole('super_admin');
+
+        // Create a super_admin user
+        $super_admin3 = User::create([
+            'first_name' => 'reyhaneh',
+            'last_name' => 'kian',
+            'phone_number' => '9190365836',
+            'email' => 'reyhaneh@gmail.com',
+            'password' => 'Reyhaneh1234'
+        ]);
+
+        // Assign super_admin role to the new user
+        $super_admin3->assignRole('super_admin');
+
+        // Create a super_admin user
+        $super_admin4 = User::create([
+            'first_name' => 'mahtab',
+            'last_name' => 'khajir',
+            'phone_number' => '9225407221',
+            'email' => 'mahtab@gmail.com',
+            'password' => 'Mahtab1234'
+        ]);
+
+        // Assign super_admin role to the new user
+        $super_admin4->assignRole('super_admin');
 
     }
 }
