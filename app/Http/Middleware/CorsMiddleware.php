9a0a6e626b30785fa03aa72e9df4fa47cdb1e1d2
mohammad <mohammadmokhtari.a1@gmail.com>
2024-08-04 00:17:15 +0330

    add cors middleware for resolve login problem

---
+++ app/Http/Middleware/CorsMiddleware.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class CorsMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle($request, Closure $next)
+    {
+        $response = $next($request);
+
+        $response->headers->set('Access-Control-Allow-Origin', 'http://localhost:3000');
+        $response->headers->set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+        $response->headers->set('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+        $response->headers->set('Access-Control-Allow-Credentials', 'true');
+
+        if ($request->getMethod() == "OPTIONS") {
+            return response('', 200)->header('Access-Control-Allow-Origin', 'http://localhost:3000')
+                                    ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+                                    ->header('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+                                    ->header('Access-Control-Allow-Credentials', 'true');
+        }
+
+        return $response;
+    }
+}
