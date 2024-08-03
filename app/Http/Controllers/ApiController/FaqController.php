58fa0ce682de906c8d4363137f4acb1a001e2ba8
Mohammad Amin <mhmdghrmn78@gmail.com>
2024-08-04 01:54:31 +0330

    created faq model

---
+++ app/Http/Controllers/ApiController/FaqController.php
@@ -0,0 +1,68 @@
+<?php
+namespace App\Http\Controllers\ApiController;
+use App\Http\Controllers\Controller;
+use App\Models\Faq;
+use Illuminate\Http\Request;
+
+class FaqController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $faq = Faq::all();
+        return $this->responseService->success_response($faq);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function store(Request $request)
+    {
+        $faq = Faq::create($request->toArray());
+        return $this->responseService->success_response($faq);
+
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function show(Request $request,$id)
+    {
+        $faq = Faq::find($id);
+        if(!$faq) {
+            return response()->json(['message' => 'Faq not found'], 404);
+        }
+        return $this->responseService->success_response($faq);
+
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Faq $id)
+    {
+        $faq = Faq::find($id);
+        if(!$faq) {
+            return response()->json(['message' => 'Faq not found'], 404);
+        }
+        $faq->update();
+        return $this->responseService->success_response($faq);
+
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Faq $id)
+    {
+        $faq = Faq::find($id);
+        if(!$faq) {
+            return response()->json(['message' => 'Faq not found'], 404);
+        }
+        $faq->delete();
+        return $this->responseService->success_response($faq);
+
+    }
+}
---
+++ routes/api/FaqRoutes.php
@@ -0,0 +1,12 @@
+<?php
+
+use App\Http\Controllers\ApiController\FaqController;
+use Illuminate\Support\Facades\Route;
+
+
+Route::group(['prefix' => 'faq','as'=> 'faq.'] ,function () {
+    Route::get('/', [FaqController::class, 'index'])->name('index');
+    Route::post('/', [FaqController::class, 'store'])->name('store');
+    Route::put('update/{id}', [FaqController::class, 'update'])->name('update');
+    Route::delete('destroy/{id}', [FaqController::class, 'destroy'])->name('destroy');
+});
