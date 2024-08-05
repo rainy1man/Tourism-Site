760c347230b436cf2c6b45bcc150c928e62940b6
Mohammad Amin <mhmdghrmn78@gmail.com>
2024-08-05 13:32:38 +0330

    final edit  setting model

--- database/migrations/2024_06_29_152736_create_settings_table.php
+++ database/migrations/2024_06_29_152736_create_settings_table.php
@@ -14,8 +14,7 @@ return new class extends Migration
         Schema::create('settings', function (Blueprint $table) {
             $table->id();
             $table->string('key');
-            $table->longText('value');
-            $table->enum('type',['fixed','banner','FAQ']);
+            $table->longText('value')->nullable();
             $table->timestamps();
         });
     }
--- routes/api/SettingRoutes.php
+++ routes/api/SettingRoutes.php
@@ -6,5 +6,5 @@ use Illuminate\Support\Facades\Route;
 Route::group(['prefix' => 'settings', 'as' => 'settings.'], function() {
     Route::get('index', [SettingController::class, 'index'])->name('index');
     Route::get('show/{id}', [SettingController::class, 'show'])->name('show')->withoutMiddleware('auth:sanctum');
-    Route::put('update/{id}', [SettingController::class, 'update'])->name('update');
+    Route::post('update/{id}', [SettingController::class, 'update'])->name('update');
 });
