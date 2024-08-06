601b1cd360cd4037fccc1060a123e4dce26a934c
Mohammad Amin <mhmdghrmn78@gmail.com>
2024-08-07 02:30:26 +0330

    edit favorite route

--- routes/api/FavoriteRoutes.php
+++ routes/api/FavoriteRoutes.php
@@ -5,6 +5,5 @@ use App\Http\Controllers\ApiController\FavoriteController;
 
 Route::group(['prefix'=> 'favorite', 'as' => 'favorite.' , 'middleware'=> 'auth:sanctum'], function (){
     Route::get('/',[FavoriteController::class, 'index'])->name('index');
-    Route::get('/{id}',[FavoriteController::class, 'show'])->name('show');
     Route::post('updateOrCreate',[FavoriteController::class, 'updateOrCreate'])->name('updateOrCreate');
 });
